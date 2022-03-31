d3c25ca7b274c8e220cdfa6f99f1f7028d921dc6
cherryry <cherryry@uwplatt.edu>
2022-03-30 22:03:16 -0500

    added initial project and created the base game such that two players can play using the console.

---
+++ AI_HW2/Program.cs
@@ -0,0 +1,154 @@
+﻿namespace HW2
+{
+    class HW2
+    {
+        private static char[] board;
+        public static void Main()
+        {
+            board = new char[9] { 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O' };
+
+            printBoard(board);
+            Console.WriteLine();
+
+            playGame(board);
+        }
+
+        private static void playGame(char[] board)
+        {
+            initializeBoard(board);
+            bool gameOver = false;
+            char playerTurn = 'W';
+            while (!gameOver)
+            {
+                board = takeTurn(board, playerTurn);
+                printBoard(board);
+                gameOver = isWin(board, playerTurn);
+
+                if (playerTurn == 'W')
+                    playerTurn = 'B';
+                else
+                    playerTurn = 'W';
+            }
+
+            // Must be inverted since the current player's turn gets updated after placement.
+            if (playerTurn == 'B')
+                Console.WriteLine("Player 1 (White) wins!");
+            else
+                Console.WriteLine("Player 2 (Black) wins!");
+        }
+
+        private static char[] takeTurn(char[] board, char player)
+        {
+            if (player == 'W')
+                Console.Write("Player 1 (White) please select an index to move a piece from: ");
+            else
+                Console.Write("Player 2 (Black) please select an index to move a piece from: ");
+
+            // Read in what piece to move.
+            int indexFrom = Convert.ToInt32(Console.ReadLine());
+
+            // Check if the index to move from is within the board's range and contains one of the player's pieces.
+            while (indexFrom < 1 || indexFrom > 9 || board[indexFrom - 1] != player)
+            {
+                if (indexFrom < 1 || indexFrom > 9)
+                    Console.Write("That index was out of bounds! Please input a valid board index to move a piece from: ");
+                else
+                    Console.Write("That index doesn't contain one of your pieces! Please input a valid board index to move a piece from: ");
+
+                indexFrom = Convert.ToInt32(Console.ReadLine());
+            }
+
+            // Read in where to move the piece to.
+            Console.Write("And an index to move to: ");
+            int indexTo = Convert.ToInt32(Console.ReadLine());
+
+            // Check if the index to move to is within the board's range and is open.
+            while (indexTo < 1 || indexTo > 9 || board[indexTo - 1] != 'O')
+            {
+                if (indexTo < 1 || indexTo > 9)
+                    Console.Write("That index was out of bounds! Please input a valid board index to move a piece to: ");
+                else
+                    Console.Write("That index is already taken! Please input a valid board index to move a piece to: ");
+
+                indexTo = Convert.ToInt32(Console.ReadLine());
+            }
+
+            // Now we can actually move the pieces.
+            board[indexFrom - 1] = 'O';
+            board[indexTo - 1] = player;
+
+            return board;
+        }
+
+        private static void printBoard(char[] board)
+        {
+            for (int i = 0; i < board.Length; i++)
+            {
+                Console.Write(board[i] + " ");
+                if ((i+1) % 3 == 0)
+                    Console.WriteLine();
+            }
+        }
+
+        private static void initializeBoard(char[] board)
+        {
+            int boardIndex;
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("Player 1 (White) please input a board index to place a piece on: ");
+                boardIndex = Convert.ToInt32(Console.ReadLine());
+
+                while (boardIndex < 1 || boardIndex > 9 || board[boardIndex - 1] != 'O')
+                {
+                    if (boardIndex < 1 || boardIndex > 9)
+                        Console.Write("That index was out of bounds! Please input a valid board index to place a piece on: ");
+                    else
+                        Console.Write("That index is already taken! Please input a valid board index to place a piece on: ");
+
+                    boardIndex = Convert.ToInt32(Console.ReadLine());
+                }
+                board[boardIndex - 1] = 'W';
+                printBoard(board);
+
+                Console.Write("Player 2 (Black) please input a board index to place a piece on: ");
+                boardIndex = Convert.ToInt32(Console.ReadLine());
+
+                while (boardIndex < 1 || boardIndex > 9 || board[boardIndex - 1] != 'O')
+                {
+                    if (boardIndex < 1 || boardIndex > 9)
+                        Console.Write("That index was out of bounds! Please input a valid board index to place a piece on: ");
+                    else
+                        Console.Write("That index is already taken! Please input a valid board index to place a piece on: ");
+
+                    boardIndex = Convert.ToInt32(Console.ReadLine());
+                }
+                board[boardIndex - 1] = 'B';
+                printBoard(board);
+            }
+        }
+
+        private static bool isWin(char[] board, char player)
+        {
+            bool isWin = false;
+            // Check for 3 in a row horizontally
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[i] == player && board[i + 3] == player && board[i + 6] == player)
+                    isWin = true;
+            }
+
+            // Check for 3 in a row vertically
+            for (int i = 0; i < 9; i+=3)
+            {
+                if (board[i] == player && board[i + 1] == player && board[i + 2] == player)
+                    isWin = true;
+            }
+
+            // Check for 3 in a row diagonally (NOT SURE IF NEEDED)
+
+            return isWin;
+        }
+    }
+
+}
+
